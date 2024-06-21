1430817e453894336d8bde2b6468161236db0e8e
RaHoni <honisuess@gmail.com>
2024-06-22 00:19:17 +0200

    Add requirements.txt and setup

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 Repo: str = "linux-surface/linux-surface"
 issueNumber: int = 91
 keywords = ["v4l2loopback", "gst-launch-1.0"]
 
 from github import Github
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     keywords = [x.lower() for x in keywords]
     git = Github(per_page=100)
@@ -18,5 +12,3 @@ if __name__ == '__main__':
     for comment in issue.get_comments():
         if any([x in comment.body.lower() for x in keywords]):
             print(comment.html_url + ": " + comment.body)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SearchGithubCommentsForKeywords',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='GPL',
+    author='RaHoni',
+    author_email='honisuess@gmail.com',
+    description='Simple script for searching GitHub issues for spezifig strings'
+)
